5d816cc4c84e2e18754ab204d7ba2f4043ac295e
Jayesh Srivastava <jayesh0200@gmail.com>
2021-06-26 23:58:23 +0530

    Update MagnifySlice.go

--- MagnifySlice.go
+++ MagnifySlice.go
@@ -1,1 +1,12 @@
 
+package main
+import "fmt"
+
+func main(){
+	fmt.Println
+}
+func enlarge(s []int, factor int) []int {
+	newslice:=make([]int, len(s)*factor)
+	copy(newslice,s)
+	return newslice
+}
